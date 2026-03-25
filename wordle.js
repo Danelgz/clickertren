6ef2445a6ecfeb9e13f346d29e51eb3fbd6d5ed5
Danelgz <Danelgzu55@gmail.com>
2026-03-25 22:27:42 +0100

    chat

--- wordle.js
+++ wordle.js
@@ -14,8 +14,6 @@ let _currentUser = null;
 // PALABRAS SECRETAS
 // ========================
 const words = [
-<<<<<<< HEAD
-=======
 "ABACO","ABAJO","ABRIR","ACERO","ACIDO","ACTOR","AGUJA","ALBUM","ALDEA","ALETA",
 "ALGAS","ALGUN","ALMAS","ALTAR","AMADO","AMIGA","AMIGO","ANDAR","ANGEL","ANIMO",
 "ANTES","APODO","ARANA","ARBOL","ARCOS","ARDOR","ARENA","ARMAS","AROMA","ARROZ",
@@ -191,175 +189,6 @@ const words = [
 "belen","pasto","bolas","grand","pugna","roger","amada","tomen","bacon","sordo",
 "amaba"
 ];
->>>>>>> 9a7b4203346fc189214c67fd822535ba4d9a145e
-
-];
-    "ABABA","ABADA","ABAIS","ABALO","ABANA","ABANO","ABARA","ABATE","ABATI","ABECE",
-"ABEJA","ABETO","ABOBA","ABOCA","ABOCO","ABOGA","ABOGO","ABONO","ABOYA","ABRAN",
-"ABRAS","ABRIA","ABRIO","ABUSO","ACABA","ACABE","ACABO","ACASO","ACATA","ACATE",
-"ACATO","ACEDE","ACEDO","ACERA","ACIAL","ACIMA","ACIMO","ACINA","ACOSA","ACOSE",
-"ACOSO","ACOTA","ACOTE","ACOTO","ACRES","ACTUA","ACUDE","ACUDI","ACUEO","ACUNA",
-"ACUNE","ACUNO","ADALA","ADAMA","ADAME","ADEMO","ADENA","ADIAS","ADIOS","ADOBE",
-"ADONA","ADONE","ADONO","ADORA","ADORE","ADORO","ADULT","AFANA","AFANE","AFANO",
-"AFEAR","AFEAS","AFEEN","AFILA","AFILE","AFILO","AFINA","AFINE","AFINO","AFUFA",
-"AGAFO","AGAVE","AGITA","AGITE","AGITO","AGORA","AGOTE","AGOTO","AGRAZ","AGRIA",
-"AGRIE","AGRIO","AGUDA","AGUDO","AGULA","AGUNA","AHITA","AHITE","AHITO","AHOGA",
-"AHOGO","AHOYA","AHOYE","AHOYO","AHUMA","AHUMO","AIREA","AIREE","AIREO","AIRES",
-"AJADA","AJADO","AJAJA","AJEAR","AJEAS","AJEEN","AJETE","AJOBA","AJOBE","AJOBO",
-"AJORA","AJORE","AJORO","AJUAR","ALABA","ALABE","ALABO","ALADA","ALADO","ALAFA",
-"ALAGA","ALAGO","ALAJA","ALAMO","ALANA","ALANO","ALAZO","ALBAL","ALBAS","ALBUR",
-"ALELA","ALELE","ALELO","ALENA","ALENO","ALERO","ALEVE","ALGOS","ALIJA","ALIJE",
-"ALIJO","ALIMA","ALIMO","ALINA","ALINE","ALINO","ALISO","ALITA","ALITE","ALITO",
-"ALIZA","ALMAS","ALOBA","ALOBE","ALOBO","ALOCA","ALOCO","ALOJA","ALOJE","ALOJO",
-"ALONA","ALOSA","ALOTA","ALOTE","ALOTO","ALOYA","ALTAR","ALTEA","ALTEE","ALTEO",
-"ALTOR","ALTOS","ALUDA","ALUDE","ALUDI","ALUDO","ALURA","ALZAR","ALZAS","AMAGA",
-"AMAGO","AMANE","AMANO","AMARA","AMARE","AMARO","AMASE","AMASO","AMEBA","AMELE",
-"AMELO","AMENA","AMENO","AMINA","AMINE","AMINO","AMITO","AMOJA","AMOJE","AMOJO",
-"AMOLA","AMOLE","AMOLO","AMONA","AMONE","AMONO","AMORA","AMORE","AMORO","AMUGA",
-"ANAFE","ANCAS","ANCHO","ANCON","ANEAS","ANEJA","ANEJO","ANELA","ANELE","ANELO",
-"ANERO","ANETO","ANGAS","ANIMA","ANIME","ANION","ANITO","ANODA","ANODE","ANODO",
-"ANOJA","ANOJE","ANOJO","ANONA","ANORE","ANORO","ANSIA","ANSIO","ANUAL","ANUDA",
-"ANUDE","ANUDO","APAGA","APAGO","APANA","APARE","APARO","APATA","APATE","APATO",
-"APELA","APELE","APELO","APENA","APENE","APENO","APERO","APICE","APILA","APILE",
-"APILO","APINA","APINE","APINO","APOCA","APOCO","APODA","APODE","APOSA","APOSE",
-"APOSO","APOYA","APOYE","APOYO","APURA","APURE","APURO","AQUEL","ARABE","ARCAS",
-"ARDEA","ARDEO","AREAS","ARELO","AREPA","ARGOT","ARIDA","ARIDO","ARILO","ARIMA",
-"ARIME","ARIMO","ARION","AROCA","AROCO","AROLA","AROLE","AROLO","ARONA","ARONE",
-"AROSA","ARRAS","ARREO","ARRIA","ARRIE","ARRIO","ASABA","ASAIS","ASARA","ASARE",
-"ASARO","ASASE","ASILA","ASILE","ASINO","ASOMA","ASOME","ASOMO","ASONA","ASONE",
-"ASONO","ASORA","ASORO","ASTIL","ATACA","ATACO","ATAJA","ATAJE","ATAJO","ATALA",
-"ATANE","ATANO","ATARA","ATARE","ATARO","ATISE","ATISO","ATOBA","ATOBE","ATOBO",
-"ATOCA","ATOCO","ATOJA","ATOJE","ATOJO","ATOLA","ATOLE","ATOLO","ATONA","ATONE",
-"ATONO","ATORA","ATORE","ATORO","ATRAE","ATRAS","ATRIL","ATROZ","ATUSA","ATUSE",
-"ATUSO","AVARA","AVARO","AVEZA","AVIDA","AVIDO","AVISA","AVISE","AYUNA","AYUNE",
-"AYUNO","AZADA","AZARE","AZUZA",
-"BABAS","BABEO","BABIA","BABOR","BACAL","BACHE","BACIA","BADEA","BAGRE","BAJAS",
-"BAJEN","BAJEL","BAJOS","BALAS","BALDA","BALDE","BALDO","BALEO","BALIN","BALSA",
-"BANAL","BANCA","BANDO","BANIO","BANJO","BANZA","BARBA","BARBO","BARDA","BARDO",
-"BARON","BASTE","BASTO","BATAN","BATEA","BATEL","BATIR","BATON","BAYAR","BAYAS",
-"BAZAR","BEATA","BEATO","BEBAS","BEBIO","BELAR","BELEN","BELIO","BERMA","BERRO",
-"BESEN","BESES","BESOS","BETEL","BETAS","BIELA","BILIS","BIRLA","BIRLE","BIRLO",
-"BISON","BITOR","BIZCA","BIZCO","BLEDO","BOCAL","BOCAS","BOCEL","BOCIO","BODAS",
-"BOLDO","BOLES","BOLLO","BOLOS","BOLSO","BONOS","BONUS","BORAX","BORCO","BORDO",
-"BORON","BORSA","BOSAR","BOTAS","BOTAR","BOTON","BOXEO","BRACO","BRAGA","BREZO",
-"BRIDA","BRIOS","BROCA","BROTE","BROZA","BRUJA","BRUJE","BRUJO","BRUMA","BRUTA",
-"BRUTO","BUDIN","BUHOS","BULLE","BULLO","BUNIO","BUQUE","BUREO","BURGO","BURIL",
-"BURLA","BURLE","BURLO","BURON","BUSCO","BUZON",
-"CABAL","CABAS","CABIO","CACHA","CACHO","CACTO","CAFRE","CAIGO","CAITA","CALCA",
-"CALCE","CALCO","CALEO","CALME","CALMO","CALVA","CALVO","CAMPA","CAMPE","CANJE",
-"CANOA","CAPON","CAPAS","CAPEA","CAPEO","CAPIS","CAPIZ","CAPSA","CARAY","CAREO",
-"CARIA","CARIO","CARPA","CARPE","CARPO","CAUCE","CAUDA","CAVAR","CAVAS","CAVIA",
-"CAVIO","CEBON","CEBRA","CEDER","CEDES","CEDIO","CEDRO","CEJAS","CELOS","CENCA",
-"CENSO","CERAS","CERCO","CEROS","CIENO","CIFRA","CIFRE","CIFRO","CITAR","CITAS",
-"CIVIL","CLAMA","CLAME","CLAMO","CLERO","CLONE","CLONO","CLUBS","COCAR","COCER",
-"COCES","COCIO","CODOS","COFIA","COGER","COIMA","COLAR","COLEO","COLES","COLMO",
-"COLPA","COLPE","COLPO","COLZA","COMAL","COMBA","COMBI","COMIO","COMPA","CONDE",
-"CONGA","COPAR","COPAS","COPEA","COPEO","COPLA","COPRA","COQUE","CORBA","COREA",
-"CORON","CORZO","COSCO","COSME","COSTE","COYOL","COZON","CRASA","CRASO","CRECE",
-"CRECI","CREDO","CRIBO","CRIOL","CROCA","CROCO","CRUCE","CRUDA","CRUDO","CRUZA",
-"CRUZE","CUAJO","CUASI","CUNEA","CUNIA","CUNIR","CUOTA","CURDA","CURDO","CURSA",
-"CURSE",
-"DAGAS","DALAS","DAMAR","DARDO","DATAR","DAUCO","DECAN","DECIA","DECOR","DEJON",
-"DELTA","DEMAS","DENSO","DESEA","DESEE","DESEO","DIANA","DICHA","DICHO","DICTO",
-"DIERA","DIGAN","DIGAS","DIMAS","DIODO","DIQUE","DIUCA","DIZMO","DOBLA","DOLCE",
-"DOLIA","DOPAR","DORAD","DORSO","DOTAR","DRAGO","DRENA","DRENE","DRENO","DRONE",
-"DUDAR","DUELO","DURAR","DUROS",
-"ECHAR","EDEMA","EGIDA","EMANA","EMANE","EMANO","EMITA","EMITE","EMITO","ENEMA",
-"ENOJA","ENOJE","ENOJO","ERIZO","ERRAR","ERRAS","ERREN","ERRES","ETAPA","ETICA",
-"ETICO","EVOCA","EVOCE","EVOCO",
-"FABLA","FALCA","FANAL","FANGO","FANON","FAROL","FARSA","FASTO","FECAL","FECHO",
-"FEBLE","FELON","FEROZ","FEUDO","FIBRA","FIDEO","FIGOS","FINJO","FISCO","FISGA",
-"FISGO","FITAS","FLAMA","FLAME","FLAMO","FLETE","FOLIO","FOLLO","FONAL","FONJE",
-"FONJO","FOQUE","FORJA","FORJE","FORJO","FORRO","FORZO","FOTON","FRAGA","FRAGO",
-"FRENO","FRESA","FRESO","FRIOL","FRISA","FRISE","FRISO","FROCE","FROGA","FUDRE",
-"FUNDA","FUNDE","FUNDI","FUNDO","FUROR",
-"GABAS","GAITA","GALGA","GALGO","GALIO","GALOP","GAMBA","GARBA","GARBO","GARRA",
-"GATAS","GAZAP","GEMIR","GIBAR","GIRAN","GIRAR","GIRON","GLERA","GLOSA","GLOSE",
-"GLOSO","GOCHO","GOLFA","GOLFO","GOLLA","GOZAN","GOZAR","GRABA","GRABE","GRABO",
-"GRIMA","GRIPE","GRIPO","GROSA","GRUMO","GRUTA","GUABA","GUACO","GUADA","GUATA",
-"GUBIA","GUECO","GUEDA","GUETO","GUIJA","GUIJE","GUIJO","GUILA","GUILE","GUILO",
-"GUIMA","GUISA","GUISE","GUISO","GURDA","GURDO","GURPA",
-"HABAR","HACHA","HACHE","HACHO","HADAR","HALDA","HALAR","HAMPA","HARBA","HARPA",
-"HARPO","HIENA","HILAR","HIMEN","HIMNO","HIPON","HISAR","HOLCO","HOLGA","HOLGO",
-"HONRA","HONRE","HONRO","HOSCA","HOSCO","HUMAN","HUMAR","HURTA","HURTE","HURTO",
-"IBERA","IBERO","ICONO","ILEON","IMAGO","INDIO","INDUE","INFLA","INFLE","INFLO",
-"INSTA","INSTE","INSTO","INVAR","ISCAR","ISLOT",
-"JALAR","JALCA","JALEO","JALMA","JAMBA","JAMBO","JAREA","JAREO","JARRA","JARRO",
-"JASPE","JEQUE","JERBO","JINCA","JINCO","JINGO","JIOTE","JODAR","JODEN","JODES",
-"JODIO","JODON","JOPAR","JORON","JOVAR","JUBAS","JUGAD","JUGAS","JULIA","JUMAR",
-"JUNCA","JUNCO","JUREL",
-"LACAR","LACAS","LACEN","LACES","LACER","LACRA","LACRE","LACRO","LACTE","LACTO",
-"LADAR","LADEN","LADOR","LADRA","LADRE","LADRO","LAICA","LAICO","LAIDO","LANAR",
-"LANCE","LANCO","LANDE","LANGA","LAQUE","LARAR","LARDE","LARDO","LAUCA","LAUCO",
-"LAUDE","LAUDO","LEBRA","LEGAN","LEGAR","LEGON","LENCA","LENCO","LEPRA","LEVAD",
-"LEVAR","LEVAS","LEXIA","LIBAR","LIBON","LICAR","LICOR","LIGAN","LIGAR","LIGUE",
-"LIMAN","LIMAR","LIMON","LITRO","LOBOS","LOCAR","LOGRA","LOGRE","LOGRO","LORAN",
-"LORZA","LOSAS","LOTES","LUCRO","LUMIA","LUMIO","LUNAS","LUPIA","LUPUS","LUTOS",
-"MACAS","MACEO","MACER","MACES","MACHA","MACHE","MACHO","MACOA","MAFIA","MAGRO",
-"MAJAR","MAJAS","MALEO","MARES","MARTA","MARTE","MASAR","MARZO","MATAN","MAUCA",
-"MAURA","MAZAN","MECEN","MECER","MECES","MECIO","MEDID","MEDIR","MEIGA","MEIGO",
-"MELAN","MELAR","MELIO","MELSA","MERAR","MERAN","MERMA","MERME","MEZCA","MEZCO",
-"MIERA","MIGAR","MIRLA","MIRLO","MISCO","MOCAR","MOCER","MOCES","MOCIO","MOHON",
-"MOJON","MOLDE","MOLER","MOLES","MOLIO","MOLLE","MOLON","MONAR","MONDA","MONDE",
-"MONDO","MORAN","MORAR","MORAZ","MORBO","MORCO","MORRA","MORRO","MORSA","MORSE",
-"MOSTO","MOTIS","MOVIA","MOZOS","MUGIR","MUGIO","MULTA","MUSCA","MUSGO","MUTIS",
-"NABAL","NABAN","NABAR","NABAS","NABOA","NACAS","NACEN","NACER","NACES","NACIB",
-"NACIO","NACOS","NAFRA","NAGUA","NAPAR","NAPAS","NARDO","NARES","NARRA","NARRE",
-"NARRO","NASAR","NASAS","NECAR","NECEA","NECIO","NELAR","NEMOS","NERON","NETOS",
-"NEVAR","NIMBA","NIMBE","NIMBO","NINFA","NITOR","NOCAR","NODAL","NODOS","NOGAL",
-"NOPAL","NORMA","NOVAR","NOVAS","NOVEL","NUBLE","NUBLO","NUCAS","NULAS","NULOS",
-"NUTRI","NUTRA","NUTRE",
-"OBESA","OBESO","OBRAR","OBSTA","OBSTE","OBSTO","OCENA","OCOTE","OCRES","ODIAR",
-"ODIAS","ODIOS","OFITA","OGROS","OJEAR","OJEAS","OJEEN","OJETE","OLEOS","OLERA",
-"OLMOS","ONDAS","ONEAS","ONOTO","OPACA","OPACO","OPTAR","OPTAS","OPTEN","ORATE",
-"ORUGA","OSEAS","OSEOS","OSTIA","OTEAR","OTEAS","OVEJA","OVERO","OVNIS",
-"PACER","PACES","PACIA","PACIO","PACOS","PAILA","PALCA","PALCO","PALMA","PALMO",
-"PALPA","PALPE","PALPO","PANAL","PANDO","PANEL","PAPAN","PAPEA","PAPEO","PARCO",
-"PARMO","PARRA","PARRO","PASEN","PASMO","PAVON","PAYOR","PAYAR","PAYAS","PAYES",
-"PECAS","PECAR","PEDAL","PEDAS","PENAS","PENCA","PENDE","PENDO","PENON","PERLA",
-"PERNA","PERNO","PEROS","PESCA","PEZON","PICAR","PICAS","PICEL","PICOL","PICON",
-"PILAF","PILAR","PINAS","PINON","PINZA","PIOJO","PIPAS","PIQUE","PISAR","PISAS",
-"PITON","PLAZO","PLEGA","PLENA","PLENO","POLAR","POLCA","POLEA","PONCA","PONCO",
-"PONYA","PORCA","PORCO","PORRA","PORTO","POSAR","POZOS","PRESA","PROAS","PROBA",
-"PROBE","PROBO","PROCO","PROLE","PRONA","PRONE","PRONO","PRORA","PROTO","PUBES",
-"PUCHA","PUCHO","PUDOR","PUGNA","PUGNE","PUGNO","PULGA","PULPO","PULSA","PUNZA",
-"PUNAL","PURGA","PURGE","PURGO",
-"RABAS","RABEL","RABIA","RABIE","RABIO","RABON","RACAS","RACHO","RAHEZ","RAJAR",
-"RAJAS","RAJON","RALEA","RALEO","RAMPA","RANDA","RAPAR","RAPAS","RAPON","RAPTO",
-"RASCA","RASPE","RASPO","RASTA","RAUDO","REDES","REMAN","REMAR","REMAS","RENAL",
-"RENCO","REPAS","REPOL","REPOS","RETAL","RETRO","REUMA","REZAN","REZAS","RIBAS",
-"RICAS","RIEGA","RIELA","RIELE","RIELO","RIGOR","RIMAS","RISCO","RISOS","RITOS",
-"RIZAR","RIZAS","ROBAS","ROBEN","ROBES","RODAL","RODAR","RODAS","RONCA","RONCE",
-"RONCO","RONZA","ROPAS","ROPEL","ROSAR","ROSAS","ROSCA","ROSCO","ROTAR","ROTAS",
-"ROTON","ROTES","ROTOR","RUBIO","RUGIR","RUGIS","RUGIO","RULOS","RUSAS",
-"SABIA","SADHE","SAGAZ","SALAZ","SALDO","SALGA","SALGO","SALMA","SALMO","SALPA",
-"SALPE","SALPO","SALSE","SALVA","SALVE","SALVO","SAMIA","SANEA","SANEO","SARNA",
-"SECAR","SECAS","SECHE","SEGAL","SEGAR","SEIDE","SELLO","SENSO","SEQUE","SIGMA",
-"SIRGA","SIRGE","SIRGO","SISAL","SISAR","SISAS","SITAR","SOBRA","SOCAZ","SOCIA",
-"SOLES","SOLIO","SONAR","SOPAS","SOPOR","SOPON","SORAL","SORBA","SORBE","SORBI",
-"SORBO","SORGO","SORJA","SORTA","SOZON","SUDAR","SUDAS","SUELE","SUMEN","SUMAR",
-"SUMOS","SURCO","SURJA","SURJO","SURTO",
-"TABAL","TACAR","TACAS","TACEO","TAFIA","TAIGA","TAJAR","TALAS","TALCA","TALCO",
-"TALDO","TALEO","TALLO","TALON","TALPA","TAMAL","TANGA","TAPAS","TAPAR","TAPON",
-"TAQUE","TARAR","TARAS","TARCO","TARJE","TARJO","TARSA","TASCA","TEDIO","TEJON",
-"TELOS","TENOR","TEOSA","TERCO","TERMA","TERMO","TESAR","TESON","TETIS","TINCA",
-"TINCO","TINEA","TINGO","TINIA","TIRSO","TITAN","TOCHE","TOCON","TOKIO","TOLDO",
-"TONCO","TONGA","TOPAS","TOPAR","TOPER","TOPIL","TOPON","TORAX","TORAR","TORBA",
-"TORCO","TORCA","TORDO","TORIL","TORON","TORPE","TORSO","TOSER","TOSES","TOSIO",
-"TRABA","TRAER","TRAPO","TRAZA","TRACE","TREPA","TRETA","TRIAR","TRIGA","TRIGO",
-"TRIPA","TROCA","TROCO","TRONA","TRONE","TROVA","TRUFA","TRUJA","TUCAN","TUNAR",
-"TURBA",
-"UBRES","ULULO","UNCIR","UNCIA","UNGIR","UNGIO","URDIR","URGIR","URGIO","USURA",
-"UTERO","UVERO",
-"VACUA","VACUO","VAGAL","VAGAR","VAGAS","VAGOS","VAHAR","VAHOS","VAJON","VANAL",
-"VARCO","VARIA","VARIO","VASAR","VASOS","VAYAN","VEDAR","VEDAS","VEJEZ","VELAR",
-"VELAS","VELON","VELOZ","VENAL","VENDI","VENDO","VENIA","VERAZ","VERGA","VERSO",
-"VETAS","VETAR","VICAR","VIGAS","VIGOR","VIRAR","VIRAS","VIRGO","VIRIL","VIVAR",
-"VIVAS","VIVAZ","VOCAS","VOCEO","VOLCA","VOLCO","VOLVA","VULGO",
-"YANKI","YEDRA","YEGUA","YERBA","YERMO","YESCA","YUGOS","YUNTA",
-"ZAINO","ZALEO","ZANCA","ZANON","ZARCO","ZARPA","ZARPE","ZARPO","ZARZA","ZOCAL",
-"ZOCLO","ZONAS","ZURCE","ZURCI","ZURRA"
-];
 
 // Función de normalización: quita tildes, convierte Ñ→N para comparación interna
 function normalize(str) {
@@ -377,15 +206,15 @@ function normalizeNoAccents(str) {
         .replace(/[\u0300-\u036f]/g, "");
 }
 
-// Construimos el set con entradas normalizadas (sin tildes, con Ñ conservada)
-const validWordsSet = new Set(rawValidWords.map(w => normalizeNoAccents(w)));
-
 // ========================
 // CLASE WORDLEGAME
 // ========================
 class WordleGame {
     constructor(words) {
-        this.words = words.map(w => normalizeNoAccents(w)).filter(w => w.length === 6);
+        this.words = words.map(w => normalizeNoAccents(w)).filter(w => w.length === 5);
+        
+        // Construimos el set con entradas normalizadas (sin tildes, con Ñ conservada)
+        this.validWordsSet = new Set(this.words.map(w => normalizeNoAccents(w)));
         // Datos cargados desde Firestore por loadStats()
         this.level = 1;
         this.totalWins = 0;
@@ -492,7 +321,7 @@ class WordleGame {
         const boardState = [];
         for (let r = 0; r < 6; r++) {
             const rowState = [];
-            for (let c = 0; c < 6; c++) {
+            for (let c = 0; c < 5; c++) {
                 if (this.board[r] && this.board[r][c]) {
                     rowState.push({
                         text: this.board[r][c].textContent,
@@ -520,7 +349,7 @@ class WordleGame {
     restoreBoardState() {
         if (this.savedBoardState) {
             for (let r = 0; r < 6; r++) {
-                for (let c = 0; c < 6; c++) {
+                for (let c = 0; c < 5; c++) {
                     if (this.savedBoardState[r] && this.savedBoardState[r][c]) {
                         const tile = this.board[r][c];
                         const state = this.savedBoardState[r][c];
@@ -559,7 +388,7 @@ class WordleGame {
         this.board = [];
         for (let r = 0; r < 6; r++) {
             const row = [];
-            for (let c = 0; c < 6; c++) {
+            for (let c = 0; c < 5; c++) {
                 const tile = document.createElement('div');
                 tile.classList.add('tile');
                 tile.dataset.row = r;
@@ -653,7 +482,7 @@ class WordleGame {
     isValidWord(guess) {
         // Normalizar sin tildes, manteniendo Ñ
         const norm = normalizeNoAccents(guess.toUpperCase());
-        return validWordsSet.has(norm);
+        return this.validWordsSet.has(norm);
     }
 
     updateCurrentRowClass() {
@@ -689,7 +518,7 @@ class WordleGame {
             return; 
         }
         if (key === "DEL") { this.deleteLetter(); return; }
-        if (this.currentCol < 6) {
+        if (this.currentCol < 5) {
             const tile = this.board[this.currentRow][this.currentCol];
             tile.textContent = key;
             tile.classList.add('filled');
@@ -709,13 +538,13 @@ class WordleGame {
     }
 
     checkWord() {
-        if (this.currentCol < 6) {
-            this.showMessage('✏️ Escribe 6 letras');
+        if (this.currentCol < 5) {
+            this.showMessage('✏️ Escribe 5 letras');
             return;
         }
 
         let guess = "";
-        for (let c = 0; c < 6; c++) guess += this.board[this.currentRow][c].textContent;
+        for (let c = 0; c < 5; c++) guess += this.board[this.currentRow][c].textContent;
 
         if (!this.isValidWord(guess)) {
             this.showMessage('❌ Palabra no válida');
@@ -734,10 +563,10 @@ class WordleGame {
             letterCount[l] = (letterCount[l] || 0) + 1;
         }
 
-        const results = new Array(6).fill('absent');
+        const results = new Array(5).fill('absent');
 
         // Primero: correctas
-        for (let c = 0; c < 6; c++) {
+        for (let c = 0; c < 5; c++) {
             if (guessNorm[c] === wordNorm[c]) {
                 results[c] = 'correct';
                 letterCount[guessNorm[c]]--;
@@ -745,7 +574,7 @@ class WordleGame {
         }
 
         // Segundo: presentes
-        for (let c = 0; c < 6; c++) {
+        for (let c = 0; c < 5; c++) {
             if (results[c] === 'correct') continue;
             const letter = guessNorm[c];
             if (letterCount[letter] > 0) {
@@ -755,7 +584,7 @@ class WordleGame {
         }
 
         // Animación reveal
-        for (let c = 0; c < 6; c++) {
+        for (let c = 0; c < 5; c++) {
             const tile = this.board[this.currentRow][c];
             const keyChar = guess[c]; // char original que el user escribió (puede ser Ñ)
             const result = results[c];
